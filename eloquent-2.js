2d207d7daff1480fea12db4340ba383b96a92308
dazyang <dazyang.contact@gmail.com>
2017-08-14 15:46:37 +1200

    Add eloquent-2.js

--- eloquent-2.js
+++ eloquent-2.js
@@ -20,9 +20,12 @@ for (var n = 1; n <= 100; n++) {
 /* Chess board
 Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a “#” character. The characters should form a chess board. */
 
+/* Define a variable size = 8 and change the program so that it works for any size */
 var size = 8;
+/*The string can be built by starting with an empty one ("") and repeatedly adding characters.*/
 var chessBoard = "";
-
+//To work with two dimensions, you will need a loop inside of a loop.
+/* You’ll need two variables to track your progress. To know whether to put a space or a hash sign at a given position, you could test whether the sum of the two counters is even (% 2). */
 for (var i = 0; i < size; i++) {
   for (var j = 0; j < size; j++) {
     if ((i + j) % 2 == 0)
@@ -30,7 +33,8 @@ for (var i = 0; i < size; i++) {
     else
     chessBoard += "#";
   }
+//A newline character is written "\n".
   chessBoard += "\n";
 }
-
+//Use console.log to inspect the output of your program.
 console.log(chessBoard);
